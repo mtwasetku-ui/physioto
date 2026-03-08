264630886f821a86e38b5a3b4828969d7941af29
mtwasetku-ui <mtwaset.ku@gmail.com>
2026-03-08 21:22:42 +1030

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,500;0,600;0,700;1,500;1,600&display=swap"
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
